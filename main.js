25f8709f45084e24a4a17a93c3f921307fb679cf
Ali Jumani <159873336+alijumani94@users.noreply.github.com>
2024-04-22 23:12:56 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+var fav_color = "white";
+//console.log(fav_color);
+var sanaJaved = "Sana ";
+var shoaibMalik = "Shoaib Malik";
+var GotMarried = (sanaJaved + shoaibMalik);
+console.log(GotMarried);
---
+++ main.ts
@@ -0,0 +1,6 @@
+let fav_color= "white";
+//console.log(fav_color);
+let sanaJaved= "Sana ";
+let shoaibMalik= "Shoaib Malik";
+let GotMarried= (sanaJaved+shoaibMalik);
+console.log(GotMarried);
